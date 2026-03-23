4fccce9ee690abb30230e2098ea79c4da3b79b10
Aarya Awasthi <aaryaawasthi06@gmail.com>
2026-03-24 00:35:09 +0530

    removed camera for deployment

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-from camera import detect_mood
 import streamlit as st
 from mood import get_mood
 from recommendation import get_recommendation
@@ -6,15 +5,6 @@ from recommendation import get_recommendation
 st.title("🧠 Mood-Based Study & Music AI")
 
 st.write("Enter your current mood 👇")
-if st.button("📸 Detect Mood from Camera"):
-    mood = detect_mood()
-    result = get_recommendation(mood)
-
-    st.subheader("📸 Camera Result:")
-    st.write("Detected Mood:", mood)
-    st.write("📚 Study:", result["study"])
-    st.write("🎧 Music:", result["music"])
-    st.markdown(f"[🎧 Play Music]({result['link']})")
 
 user_input = st.text_input("Your Mood:")
 
